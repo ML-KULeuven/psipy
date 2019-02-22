0357edd12be330da5fb1f345a121284b14823b14
pedrozudo <pedrozuidbergdosmartires@gmail.com>
2019-02-22 12:13:06 +0100

    added iveron filter

--- psipy/psipy.d
+++ psipy/psipy.d
@@ -161,8 +161,9 @@ auto integrate(string[] variables, dexpr.DExpr integrand){
       integral = integrate_simple(variables[i], integral);
       /*integral = integral.simplify(one);*/
    }
-
    return integral.simplify(one);
+
+   /*return integral.simplify(one);*/
 }
 
 
@@ -176,28 +177,32 @@ auto integrate_poly(string[] variables, dexpr.DExpr integrand){
    auto integral = integrand;
    foreach (i; 0 ..variables.length){
       /*writeln(variables[i]);*/
-      integral = integrate_poly_simple(variables[i], integral);
+      /*writeln(integral);*/
+      integral = integrate_poly_simple1(variables[i], integral);
       /*integral = integral.simplify(one);*/
       /*writeln("");*/
    }
+   /*writeln("");*/
+   /*writeln(integral);*/
+   /*writeln("simplifying");*/
+   /*writeln(integral);*/
+   integral = integral.simplify(one);
+   writeln(integral);
 
-   return integral.simplify(one);
+   /*writeln(integral);*/
+   return integral;
 }
 
 
-auto integrate_poly_simple(string variable, dexpr.DExpr integrand){
+auto integrate_poly_simple1(string variable, dexpr.DExpr integrand){
    auto v = variable.dVar;
    integrand = make_closed_bounds(integrand);
-   /*auto polytope_integrals = build_polytop_integrals(integrand,v);*/
-   /*auto result  = zero;
-   foreach(pi;polytope_integrals){
-      writeln(pi[0]);
-      writeln(pi[1]);
-      result = result + dInt(v,pi[1]);
-   }*/
    auto result = dInt(v, integrand);
    return result;
 }
+
+
+
 
 
 dexpr.DExpr make_closed_bounds(dexpr.DExpr expression){
@@ -231,7 +236,66 @@ dexpr.DExpr make_closed_bounds(dexpr.DExpr expression){
    }
 }
 
-/*there are bugs in this function*/
+
+auto filter_iverson(dexpr.DExpr expression){
+   if(isPolynomial(expression)){
+      return expression;
+   }
+   else if(auto iv=cast(DIvr)expression){
+      return S("1");
+   }
+   else if(auto dsum=cast(DPlus)expression){
+      auto result = S("0");
+      foreach(s;dsum.summands()){
+         result = result+filter_iverson(s);
+      }
+      return result;
+   }
+   else if(auto dmult=cast(DMult)expression){
+      auto result = S("1");
+      foreach(f;dmult.factors()){
+         result = result*filter_iverson(f);
+      }
+      return result;
+   }
+   else{
+      return expression;
+   }
+}
+
+/+
+DExpr[3] splitCommonFactors(DExpr e1,DExpr e2){
+		auto common=intersect(e1.factors.setx,e2.factors.setx); // TODO: optimize?
+		if(!common.length) return [one,e1,e2];
+		auto e1only=e1.factors.setx.setMinus(common);
+		auto e2only=e2.factors.setx.setMinus(common);
+		return [dMult(common),dMult(e1only),dMult(e2only)];
+}
+
+
+auto integrate_poly_simple2(string variable, dexpr.DExpr integrand){
+   auto v = variable.dVar;
+   auto polytope_integrals = build_polytop_integrals(integrand,v);
+   auto result  = zero;
+   /*writeln("");*/
+   /*writeln(polytope_integrals.length);*/
+   /*writeln(v);*/
+   foreach(pi;polytope_integrals){
+      /*writeln(pi[0]);
+      writeln(pi[1]);*/
+      result = result + dInt(v,pi[0]*pi[1]);
+
+      auto common = splitCommonFactors(result,dInt(v,pi[0]*pi[1]));
+      /*writeln(common[0]);
+      writeln(common[1]);
+      writeln(common[2]);*/
+
+      result = common[0]*(common[1]+common[2]);
+   }
+   writeln(v);
+   return result;
+}
+
 dexpr.DExpr[][] build_polytop_integrals(dexpr.DExpr expression, DVar v){
    dexpr.DExpr[][] result;
 
@@ -240,166 +304,197 @@ dexpr.DExpr[][] build_polytop_integrals(dexpr.DExpr expression, DVar v){
          result ~= [S("1"),S("1")];
          return result;
       }
+      else if (iv.toString().canFind("=")){
+         result ~= [S("0"),S("0")];
+         return result;
+      }
       else if (iv.hasFreeVar(v)){
-         return result ~= [iv,iv];
+         return result ~= [expression,S("1")];
       }
       else {
          return result ~= [S("1"),iv];
       }
    }
+   else if(isPolynomial(expression)){
+      result ~= [S("1"),expression];
+      return result;
+   }
    else if(auto dsum=cast(DPlus)expression){
+      dexpr.DExpr[][] r;
       foreach(s;dsum.summands()){
-         if (result.length==0){
-            result ~= build_polytop_integrals(s,v);
-         }
-         else{
-            auto r = build_polytop_integrals(s,v);
-            foreach(t1;r){
-               for (int i = 0; i < result.length; i++){
-                  if(t1[0]==result[i][0]){
-                     result[i] = [result[i][0], result[i][1]+t1[1]];
-                     break;
-                  }
-                  else if(i==result.length-1){
-                     result ~= t1;
-                     break;
-                  }
-               }
-            }
+         r = build_polytop_integrals(s,v);
+         foreach(t;r){
+            result ~= t;
          }
       }
       return result;
    }
    else if(auto dmult=cast(DMult)expression){
-      dexpr.DExpr[string] help_result;
-      dexpr.DExpr[string] help_bounds;
-
+      dexpr.DExpr[][] r;
+      dexpr.DExpr[][] result_help;
+      result ~= [S("1"), S("1")];
       foreach(f;dmult.factors()){
-         if (result.length==0){
-            result ~= build_polytop_integrals(f,v);
-         }
-         else{
-            auto r = build_polytop_integrals(f,v);
-            help_result = null;
-            foreach(t1;r){
-               foreach(t2;result){
-                  auto bounds = (t1[0]*t2[0]).simplify(one);
-                  auto bounds_as_key = bounds.toString();
-                  if (bounds_as_key in help_result){
-                     help_result[bounds_as_key] =  (help_result[bounds_as_key]+t1[1]*t2[1]).simplify(one);
-                  }
-                  else{
-                     help_result[bounds_as_key] = (t1[1]*t2[1]).simplify(one);
-                     help_bounds[bounds_as_key] = bounds;
-                  }
-               }
-
-               result = null;
-               foreach(k;help_result.byKey){
-                  result ~= [help_bounds[k],help_result[k]];
-               }
+         r = build_polytop_integrals(f,v);
+         foreach(t1;r){
+            foreach(t2;result){
+               result_help ~= [(t1[0]*t2[0]).simplify(one), (t1[1]*t2[1]).simplify(one)];
             }
          }
+         result = null;
+         foreach(t;result_help){
+            result ~= [t[0],t[1]];
+         }
+         result_help = null;
+
       }
       return result;
    }
    else{
       return result ~= [S("1"), expression];
    }
-
-
 }
 
 
 
-/*dexpr.DExpr[][] build_polytop_integrals(dexpr.DExpr expression, DVar v){
-   dexpr.DExpr[string] result;
+
+auto integrate_poly_simple3(string variable, dexpr.DExpr integrand){
+   auto v = variable.dVar;
+   auto polytope_integrals = build_polytop_integrals2(integrand,v);
+   auto result  = zero;
+   /*writeln("");
+   writeln(polytope_integrals.length);
+   writeln(v);*/
+   foreach(pi;polytope_integrals){
+      /*writeln(pi[0]);
+      writeln(pi[1]);*/
+      /*result = (result + make_closed_bounds(dInt(v,pi[0]*pi[1])).simplify(one)*pi[2]).simplify(one);*/
+      auto inter_result = make_closed_bounds(dInt(v,pi[0]*pi[1]).simplify(one));
+      inter_result = (inter_result*pi[2]).simplify(one);
+      auto common = splitCommonFactors(result,inter_result);
+
+      /*writeln("commmon");
+      writeln(common[0]);
+      writeln(common[1]);
+      writeln(common[2]);*/
+
+      result = common[0]*(common[1]+common[2]);
+
+   }
+   /*result = make_closed_bounds(result);*/
+   /*writeln(result);*/
+   writeln(v);
+   return result;
+}
+
+dexpr.DExpr[][] build_polytop_integrals2(dexpr.DExpr expression, DVar v){
+   dexpr.DExpr[][] result;
+
 
    if(auto iv=cast(DIvr)expression){
       if (iv.toString().canFind("≠")){
-         result[one.toString()] = S("1");
+         result ~= [S("1"),S("1"),S("1")];
+         return result;
+      }
+      else if (iv.toString().canFind("=")){
+         result ~= [S("0"),S("0"),S("0")];
          return result;
       }
       else if (iv.hasFreeVar(v)){
-         iv = iv.simplify(one);
-         return result[iv.toString()] = iv;
+         result ~= [expression,S("1"),S("1")];
+         return result;
       }
       else {
-         iv = iv.simplify(one);
-         return result[one.toString()] = iv;
+         result ~= [S("1"),S("1"),iv];
+         return result;
       }
+   }
+   else if(isPolynomial(expression)){
+      if (expression.hasFreeVar(v)){
+         result ~= [S("1"),expression.simplify(one),S("1")];
+      }
+      else{
+         result ~= [S("1"),S("1"),expression.simplify(one)];
+      }
+      return result;
+   }
+   else if(auto dsum=cast(DPlus)expression){
+      dexpr.DExpr[][] r;
+      dexpr.DExpr[][] result_help;
+      dexpr.DExpr[string] result_bounds_help;
+      dexpr.DExpr[string] result_integrand_help;
+      dexpr.DExpr[string] result_rest_help;
+      string bounds_as_key;
+
+      foreach(s;dsum.summands()){
+         r = build_polytop_integrals2(s,v);
+         foreach(t;r){
+            result ~= t;
+         }
+      }
+      /*result_help = result;
+      foreach(t;result_help){
+            bounds_as_key = t[0].toString();
+            if ((bounds_as_key in result_bounds_help) && (t[2]==result_rest_help[bounds_as_key])) {
+               result_integrand_help[bounds_as_key] = result_integrand_help[bounds_as_key]+t[1];
+
+            }
+            else{
+               result_bounds_help[bounds_as_key] = t[0];
+               result_integrand_help[bounds_as_key] = t[1];
+               result_rest_help[bounds_as_key] = t[2];
+            }
+      }*/
+      return result;
    }
    else if(auto dmult=cast(DMult)expression){
-      dexpr.DExpr[string] help_result;
-      dexpr.DExpr[string] result;
+      dexpr.DExpr[][] r;
+      dexpr.DExpr[][] result_help;
+      dexpr.DExpr[string] result_bounds_help;
+      dexpr.DExpr[string] result_integrand_help;
+      dexpr.DExpr[string] result_rest_help;
+      string bounds_as_key;
 
+      result ~= [S("1"), S("1"),S("1")];
       foreach(f;dmult.factors()){
-         if (result.length==0){
-            result = build_polytop_integrals(f,v);
-         }
-         else{
-            auto r = build_polytop_integrals(f,v);
-            help_result = [][];
-            foreach(t1;result.byKey){
-               foreach(t2;r.byKey){
-                  auto bounds = (result[0]*t2[0]).simplify(one);
-                  auto bounds_as_key = bounds.toString();
-
-                  if (bounds_as_key in help_result){
-                     help_result[bounds_as_key] = t1[1]*t2[1]]
-
-                  }
-                  else{
-                     result[t1] = r[t1];
-                  }
-
-                     help_result ~= [(t1[0]*t2[0]).simplify(one), t1[1]*t2[1]];
-               }
-            }
-            result[][] = help_result;
-            /*result = [][];*/
-            /*for (int i = 0; i < help_result.length; i++){
-               bounds_as_key = help_result[]
-
-               exp2index[result[i][0].toString()] = i;
-            }*/
-            /*TODO simplify result*/
-         /*}
-      }
-      return result;
-   }*/
-
-
-   /*else if(auto dsum=cast(DSum)expression){
-      foreach(s;dsum.summands()){
-         if (result.length==0){
-            result ~= build_polytop_integrals(s,v);
-         }
-         else{
-            auto r = build_polytop_integrals(s,v);
-            foreach(t1;r){
-               for (int i = 0; i < result.length; i++){
-                  if(t1[0]==result[i][0]){
-                     result[i] = [result[i][0], result[i][1]+t1[1]];
-                     break;
-                  }
-                  else if(i==result.length-1){
-                     result ~= t1;
-                     break;
-                  }
-               }
+         r = build_polytop_integrals2(f,v);
+         foreach(t1;r){
+            foreach(t2;result){
+               result_help ~= [(t1[0]*t2[0]).simplify(one),(t1[1]*t2[1]).simplify(one),(t1[2]*t2[2]),simplify(one)];
             }
          }
+         /*foreach(t;result_help){
+               bounds_as_key = t[0].toString();
+               if ((bounds_as_key in result_bounds_help) && (t[2]==result_rest_help[bounds_as_key])) {
+                  result_integrand_help[bounds_as_key] = result_integrand_help[bounds_as_key]+t[1];
+
+               }
+               else{
+                  result_bounds_help[bounds_as_key] = t[0];
+                  result_integrand_help[bounds_as_key] = t[1];
+                  result_rest_help[bounds_as_key] = t[2];
+               }
+         }*/
+
+         result = null;
+         result = result_help;
+         /*foreach(k;result_bounds_help.byKey()){
+            result ~= [result_bounds_help[k],result_integrand_help[k],result_rest_help[k]];
+         }*/
+
+         result_help = null;
+         result_bounds_help = null;
+         result_integrand_help = null;
+         result_rest_help = null;
       }
       return result;
-   }*/
-
-   /*else{
-      return result ~= [S("1"), expression];
+   }
+   else{
+      return result ~= [S("1"), S("1"),expression];
    }
 
 
-}*/
+}
++/
 
 
 
